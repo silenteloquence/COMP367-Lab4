e80e70dbba4fd042e3e3bc650e8f47c60061c5e5
Talgat <134800440+silenteloquence@users.noreply.github.com>
2025-04-22 12:51:37 -0400

    Add files via upload

---
+++ StringUtilities/StringUtilities/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace StringUtilities
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+            return char.IsUpper(str[0]);
+        }
+    }
+}
